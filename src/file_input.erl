481b12fac74380f0bf09fc694111d5df82168cb6
Max Hirschhorn <maxh@caltech.edu>
2013-03-21 10:46:23 -0700

    Modified record for file_input module state.
    Improper to call the attribute `poll_freq' when it is a timer that
    _pokes_ the process into performing another read.
    
    Could use the timer:send_after function with a message of the form
    {'$gen_cast', {read}}, instead of setting up an interval. This may be
    necessary in order to avoid having the timer once we reach the end of
    the file.

--- src/file_input.erl
+++ src/file_input.erl
@@ -5,7 +5,7 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 
--record(state, {fd, batch_size, poll_freq}).
+-record(state, {file, batch_size, poke_freq}).
 
 start(Name, Schema, Options) ->
     gen_server:start({local, Name}, ?MODULE, Options, [])
@@ -24,15 +24,15 @@ init([], State = #state{}) ->
 ;
 
 init([{filename, Filename} | Options], State = #state{}) ->
-    init(Options, State#state{fd=file:open(Filename, [read])})
+    init(Options, State#state{file=open(Filename)})
 ;
 
 init([{batch_size, BatchSize} | Options], State = #state{}) ->
     init(Options, State#state{batch_size=BatchSize})
 ;
 
-init([{poll_freq, PollFreq} | Options], State = #state{}) ->
-    init(Options, State#state{poll_freq=PollFreq})
+init([{poke_freq, PollFreq} | Options], State = #state{}) ->
+    init(Options, State#state{poke_freq=PollFreq})
 .
 
 handle_call(_Request, _From, State) -> {reply, ok, State}.
@@ -42,13 +42,13 @@ handle_call(_Request, _From, State) -> {reply, ok, State}.
 handle_cast(
     Request = {read}
   , State = #state{
-        fd = Fd
+        file = File
       , batch_size = BatchSize
-      , poll_freq = PollFreq
+      , poke_freq = PokeFreq
     }
 ) ->
-    read(Fd, BatchSize)
-  % , timer:send_after(PollFreq, Request)
+    read(File, BatchSize)
+  % , timer:send_after(PokeFreq, Request)
 
   , {noreply, State}
 .
@@ -62,15 +62,23 @@ code_change(_OldVsn, State, _Extra) -> {ok, State}.
 %% --------------------------------------------------------------------
 
 % post_init() -> gen_server:cast(erlang:self(), {read}).
-post_init(State = #state{poll_freq = PollFreq}) ->
+post_init(State = #state{poke_freq = PokeFreq}) ->
     timer:apply_interval(
-        PollFreq
+        PokeFreq
       , ?MODULE
       , do_read
       , [erlang:self()]
     )
 .
 
-read(Fd, BatchSize) ->
+open(Filename) ->
+    file:open(Filename, [read])
+.
+
+read(IoDevice, BatchSize) ->
     io:format("Reading file...~n")
+.
+
+send(Data) ->
+    gen_event:notify(erlang:self(), {data, Data})
 .
