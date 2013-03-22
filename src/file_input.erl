bd5f1dc6305ced9b6017a800bf6d7c443bdc168d
Max Hirschhorn <maxh@caltech.edu>
2013-03-21 23:58:33 -0700

    Refactored file_input to implement plan_node behaviour.

--- src/file_input.erl
+++ src/file_input.erl
@@ -1,84 +1,101 @@
 -module(file_input).
--behaviour(gen_server).
+-behaviour(plan_node).
 
--export([start/3, do_read/1]).
--export([init/1, handle_call/3, handle_cast/2, handle_info/2,
-         terminate/2, code_change/3]).
+-export([start/3]).
+-export([init/1, delegate/2]).
 
--record(state, {file, batch_size, poke_freq}).
+-record(file_input, {io_device, batch_size, poke_freq}).
+
+%%% =============================================================== %%%
+%%%  API                                                            %%%
+%%% =============================================================== %%%
 
 start(Name, Schema, Options) ->
-    gen_server:start({local, Name}, ?MODULE, Options, [])
+    plan_node:start(Name, ?MODULE, Schema, Options)
 .
 
-% do_read() -> gen_server:cast(erlang:self(), {read}).
-do_read(Pid) -> gen_server:cast(Pid, {read}).
+%% ----------------------------------------------------------------- %%
 
-%% --------------------------------------------------------------------
+init(Options) when is_list(Options) -> init(Options, #file_input{}).
 
-init(Options) when is_list(Options) -> init(Options, #state{}).
+init([], State = #file_input{}) ->
+    post_init()
 
-init([], State = #state{}) ->
-    post_init(State)
   , {ok, State}
 ;
 
-init([{filename, Filename} | Options], State = #state{}) ->
-    init(Options, State#state{file=open(Filename)})
+init([{filename, Filename} | Options], State = #file_input{}) ->
+    init(Options, State#file_input{io_device=open(Filename)})
 ;
 
-init([{batch_size, BatchSize} | Options], State = #state{}) ->
-    init(Options, State#state{batch_size=BatchSize})
+init([{batch_size, BatchSize} | Options], State = #file_input{}) ->
+    init(Options, State#file_input{batch_size=BatchSize})
 ;
 
-init([{poke_freq, PollFreq} | Options], State = #state{}) ->
-    init(Options, State#state{poke_freq=PollFreq})
+init([{poke_freq, PollFreq} | Options], State = #file_input{}) ->
+    init(Options, State#file_input{poke_freq=PollFreq})
 .
 
-handle_call(_Request, _From, State) -> {reply, ok, State}.
-
-% handle_cast(_Request, State) -> {noreply, State}.
-
-handle_cast(
+delegate(
     Request = {read}
-  , State = #state{
-        file = File
+  , State = #file_input{
+        io_device = IoDevice
       , batch_size = BatchSize
       , poke_freq = PokeFreq
     }
 ) ->
-    read(File, BatchSize)
-  % , timer:send_after(PokeFreq, Request)
+    case read(IoDevice, BatchSize) of
+        {continue, Data} ->
+            plan_node:notify(
+                erlang:self()
+              , {'$gen_cast', {delegate, {data, Data}}}
+            )
 
-  , {noreply, State}
+          , timer:send_after(PokeFreq, {'$gen_cast', {delegate, Request}})
+
+          , {ok, State}
+
+      ; {done, Data} ->
+            plan_node:notify(
+                erlang:self()
+              , {'$gen_cast', {delegate, {data, Data}}}
+            )
+
+          , file:close(IoDevice)
+
+          , NewState = State#file_input{
+                io_device=undefined
+            }
+
+          , {ok, NewState}
+    end
+;
+
+delegate(_Request, State) ->
+    {ok, State}
 .
 
-handle_info(_Info, State) -> {ok, State}.
+%%% =============================================================== %%%
+%%%  private functions                                              %%%
+%%% =============================================================== %%%
 
-terminate(_Reason, _State) -> ok.
+post_init() -> gen_server:cast(erlang:self(), {delegate, {read}}).
 
-code_change(_OldVsn, State, _Extra) -> {ok, State}.
-
-%% --------------------------------------------------------------------
-
-% post_init() -> gen_server:cast(erlang:self(), {read}).
-post_init(State = #state{poke_freq = PokeFreq}) ->
-    timer:apply_interval(
-        PokeFreq
-      , ?MODULE
-      , do_read
-      , [erlang:self()]
-    )
+open(Filename) ->
+    {ok, IoDevice} = file:open(Filename, [read])
+  , IoDevice
 .
-
-open(Filename) -> file:open(Filename, [read]).
 
 read(IoDevice, BatchSize) ->
-    io:format("Reading file...~n")
+    read(IoDevice, BatchSize, [])
 .
 
-read(IoDevice, 0, Result) when is_list(Result) ->
-    lists:reverse(Result)
+read(_IoDevice, -1, Result) when is_list(Result) ->
+    {done, lists:reverse(Result)}
+;
+
+read(_IoDevice, 0, Result) when is_list(Result) ->
+    {continue, lists:reverse(Result)}
 ;
 
 read(IoDevice, BatchSize, Result)
@@ -86,13 +103,13 @@ read(IoDevice, BatchSize, Result)
     is_integer(BatchSize), BatchSize > 0
   , is_list(Result)
   ->
-    read(
-        IoDevice
-      , BatchSize - 1
-      , [process(io:read(IoDevice, '')) | Result]
-    )
+    case io:read(IoDevice, '') of
+        {ok, Term} ->
+            read(IoDevice, BatchSize - 1, [Term | Result])
+
+      ; eof ->
+            read(IoDevice, -1, Result)
+    end
 .
 
-process(Data) -> Data.
-
-send(Data) -> gen_event:notify(erlang:self(), {data, Data}).
+%% ----------------------------------------------------------------- %%
