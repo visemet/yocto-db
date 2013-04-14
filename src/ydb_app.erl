77feb95548c3b8a91c7007810f673e899c9109df
Max Hirschhorn <maxh@caltech.edu>
2013-04-13 22:31:13 -0700

    Created system-level in ydb_app.

--- src/ydb_app.erl
+++ src/ydb_app.erl
@@ -1,13 +1,20 @@
 -module(ydb_app).
 -behaviour(application).
 
+-export([register_input_stream/1, register_query/1]).
 -export([start/2, stop/1]).
 
 %%% =============================================================== %%%
 %%%  API                                                            %%%
 %%% =============================================================== %%%
 
+register_input_stream(Spec) ->
+    ydb_sup:register_input_stream(Spec)
+.
 
+register_query(Spec) ->
+    ydb_sup:register_query(Spec)
+.
 
 %% ----------------------------------------------------------------- %%
 
