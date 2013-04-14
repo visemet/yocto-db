6855a85e1a767c687cf41c7a638565625fd53426
Anjoola <anjoola@anjoola.com>
2013-04-14 01:06:41 -0700

    Added specification for predicates and operators to header file.

--- src/ydb_plan_node.hrl
+++ src/ydb_plan_node.hrl
@@ -1,9 +1,11 @@
 %% @author Max Hirschhorn <maxh@caltech.edu>
 
-%% @doc Header file for the plan node.
+%% @doc Header file for the plan node. Contains definitions for the
+%%      schema, predicates, and operators.
 
 -export_type([ydb_schema/0, ydb_timestamp/0]).
 
+%% A tuple containing the data.
 -record(ydb_tuple, {
     timestamp=0 :: non_neg_integer()
   , data={} :: tuple()
@@ -16,5 +18,37 @@
 ].
 
 -type ydb_timestamp() :: '$auto_timestamp' | {atom(), atom()}.
+
+%% ----------------------------------------------------------------- %%
+
+-record(ydb_and, {
+    clauses=[] :: [clause()]
+}).
+
+-record(ydb_or, {
+    clauses=[] :: [clause()]
+}).
+
+-record(ydb_not, {
+    clause :: clause()
+}).
+
+-record(ydb_cv, {
+    column :: atom()
+  , operator :: compare()
+  , value :: term()
+}).
+
+-record(ydb_cc, {
+    left_column :: atom()
+  , operator :: compare()
+  , right_column :: atom()
+}).
+
+-type clause() :: #ydb_cv{} | #ydb_cc{} | #ydb_and{} | #ydb_or{}
+                | #ydb_not{}
+.
+
+-type compare() :: 'gt' | 'lt' | 'eq' | 'lte' | 'gte' | 'ne'.
 
 %% ----------------------------------------------------------------- %%
