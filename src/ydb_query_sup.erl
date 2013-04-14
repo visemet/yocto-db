ccddb98caf8ba57cd6dce42f5eb6ab3bf6b100f9
Max Hirschhorn <maxh@caltech.edu>
2013-04-13 22:01:18 -0700

    Created ydb_query_sup module.
    Implements supervisor behaviour (for queries). Supports registering a
    query via register/2. Currently requires a name be given to the query
    to recognize it internally.

---
+++ src/ydb_query_sup.erl
@@ -0,0 +1,55 @@
+%% @author Max Hirschhorn <maxh@caltech.edu>
+
+%% @doc TODO 
+-module(ydb_query_sup).
+-behaviour(supervisor).
+
+-export([start_link/0, register/2]).
+-export([init/1]).
+
+%%% =============================================================== %%%
+%%%  API                                                            %%%
+%%% =============================================================== %%%
+
+%-spec TODO
+
+%% @doc TODO
+start_link() ->
+    supervisor:start_link(?MODULE, {})
+.
+
+%-spec TODO
+
+%% @doc TODO
+register(Supervisor, {Name, Query}) ->
+    supervisor:start_child(
+        Supervisor
+
+      , {
+            Name
+          , {ydb_query, start_link, [Query]}
+
+          , transient
+          , infinity % supervisor
+          , worker
+          , [ydb_query]
+        }
+    )
+.
+
+%% ----------------------------------------------------------------- %%
+
+%-spec TODO
+
+%% @doc TODO
+init({}) ->
+    {ok, {{one_for_one, 1, 60}, []}}
+.
+
+%%% =============================================================== %%%
+%%%  private functions                                              %%%
+%%% =============================================================== %%%
+
+
+
+%% ----------------------------------------------------------------- %%
