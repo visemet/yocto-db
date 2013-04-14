7b1dbaed933c54bbe3f39eddf9874682054c42be
Max Hirschhorn <maxh@caltech.edu>
2013-04-13 22:24:01 -0700

    Updated ydb_sup module.
    Supervises two processes: (1) a process for supervising all input
    streams, and (2) a process for supervising all queries. Registers the
    names of these processes, so the user has an easier way to interface
    with the system by only needing to keep track of the main supervisor
    process.

--- src/ydb_sup.erl
+++ src/ydb_sup.erl
@@ -1,21 +1,54 @@
 -module(ydb_sup).
 -behaviour(supervisor).
 
--export([start_link/0]).
+-export([start_link/0, register_input_stream/1, register_query/1]).
 -export([init/1]).
+
+-define(INPUT_STREAM_SUP, ydb_input_stream_sup).
+-define(QUERY_SUP, ydb_query_sup).
 
 %%% =============================================================== %%%
 %%%  API                                                            %%%
 %%% =============================================================== %%%
 
 start_link() ->
-    supervisor:start_link(?MODULE, [])
+    supervisor:start_link(?MODULE, {})
+.
+
+register_input_stream(Spec) ->
+    ydb_input_stream_sup:register(?INPUT_STREAM_SUP, Spec)
+.
+
+register_query(Spec) ->
+    ydb_query_sup:register(?QUERY_SUP, Spec)
 .
 
 %% ----------------------------------------------------------------- %%
 
-init([]) ->
-    {ok, {{one_for_one, 1, 60}, []}}
+init({}) ->
+    {ok, {
+        {one_for_one, 1, 60}
+
+      , [
+            {
+                ?INPUT_STREAM_SUP
+              , {?INPUT_STREAM_SUP, start_link, [?INPUT_STREAM_SUP]}
+              , transient
+              , infinity % supervisor
+              , supervisor
+              , [?INPUT_STREAM_SUP]
+            }
+
+          , {
+                ?QUERY_SUP
+              , {?QUERY_SUP, start_link, [?QUERY_SUP]}
+              , transient
+              , infinity % supervisor
+              , supervisor
+              , [?QUERY_SUP]
+            }
+        ]
+    }}
 .
 
 %%% =============================================================== %%%
