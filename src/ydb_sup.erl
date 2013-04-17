b609b59bb594f2cbc6894b01a1d47122f5c26c83
Max Hirschhorn <maxh@caltech.edu>
2013-04-17 08:31:25 -0700

    Register top supervisor with name locally, as ydb.

--- src/ydb_sup.erl
+++ src/ydb_sup.erl
@@ -11,20 +11,32 @@
 %%%  API                                                            %%%
 %%% =============================================================== %%%
 
+%-spec
+
+%% @doc TODO
 start_link() ->
-    supervisor:start_link(?MODULE, {})
+    supervisor:start_link({local, ydb}, ?MODULE, {})
 .
 
+%-spec
+
+%% @doc TODO
 register_input_stream(Spec) ->
     ydb_input_stream_sup:register(?INPUT_STREAM_SUP, Spec)
 .
 
+%-spec
+
+%% @doc TODO
 register_query(Spec) ->
     ydb_query_sup:register(?QUERY_SUP, Spec)
 .
 
 %% ----------------------------------------------------------------- %%
 
+%-spec
+
+%% @doc TODO
 init({}) ->
     {ok, {
         {one_for_one, 1, 60}
