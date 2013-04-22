1b831d5834b400e4f4928cbdb63bc980ec5d0724
Max Hirschhorn <maxh@caltech.edu>
2013-04-21 18:23:35 -0700

    Defined file output node structure in ydb_planner module.

--- src/ydb_planner.erl
+++ src/ydb_planner.erl
@@ -92,6 +92,40 @@ make_no_branch(
   , make_no_branch(Next, CurrId, NewHistory, [ChildSpec|Result])
 ;
 
+make_no_branch(
+    {Type = file_output, Next, Filename}
+  , PrevId
+  , History
+  , Result
+) when
+    is_tuple(Next)
+  , is_atom(PrevId) orelse is_tuple(PrevId)
+  , is_list(Result)
+  ->
+    CurrId = case dict:find(Type, History) of
+        {ok, Value} -> {Type, Value}
+
+      ; error -> {Type, 0}
+    end
+
+  , Listen = if
+        % Listens to an input stream
+        is_atom(PrevId) -> PrevId
+
+        % Listens to another node in the query
+      ; is_tuple(PrevId) -> ydb_sup_utils:pid_fun(PrevId)
+    end
+
+  , ChildSpec = prepare_child_spec(CurrId, {ydb_file_output, start_link, [
+        [{filename, Filename}]
+      , [{listen, [Listen]}]
+    ]})
+
+  , NewHistory = dict:update_counter(Type, 1, History)
+
+  , make_no_branch(Next, CurrId, NewHistory, [ChildSpec|Result])
+;
+
 make_no_branch(Query, _PrevId, _History, _Result) ->
     {error, {badarg, Query}}
 .
