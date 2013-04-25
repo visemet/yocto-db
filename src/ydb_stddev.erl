df7f926dd7551ac3644d4fb9f624c46683b57f5b
Anjoola <anjoola@anjoola.com>
2013-04-24 19:28:27 -0700

    Added STDDEV. - Fixed a comment in VAR.

--- src/ydb_stddev.erl
+++ src/ydb_stddev.erl
@@ -1,8 +1,8 @@
 %% @author Angela Gong <anjoola@anjoola.com>
 
-%% @doc Module for the VAR aggregate function. Finds the variance of
-%%      the data so far.
--module(ydb_var).
+%% @doc Module for the STDDEV aggregate function. Finds the standard
+%%      deviation of the data so far.
+-module(ydb_stddev).
 -behaviour(ydb_plan_node).
 
 -export([start_link/2, start_link/3]).
@@ -23,7 +23,7 @@
 % curr_M2 is M2 as found in the online_variance function as described in
 % http://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#
 %     On-line_algorithm
--record(aggr_var, {
+-record(aggr_stddev, {
     column :: atom() | {atom(), atom()}
   , index :: integer()
   , curr_count=0 :: integer()
@@ -31,20 +31,20 @@
   , curr_mean :: float()
 }).
 
--type aggr_var() :: #aggr_var{
+-type aggr_stddev() :: #aggr_stddev{
     column :: undefined | atom() | {atom(), atom()}
   , index :: undefined | integer()
   , curr_count :: integer()
   , curr_M2 :: float()
   , curr_mean :: undefined | float()}.
-%% Internal var aggregate state.
+%% Internal stddev aggregate state.
 
 -type option() ::
     {column, Column :: atom() | {ColName :: atom(), NewName :: atom()}}.
-%% Options for the VAR aggregate:
+%% Options for the STDDEV aggregate:
 %% <ul>
 %%   <li><code>{column, Column}</code> - The column name to find the
-%%       variance of. <code>Column</code> is either an atom
+%%       standard deviation of. <code>Column</code> is either an atom
 %%       <code>Column</code> which is the name of the column, or the
 %%       tuple <code>{ColName, NewName}</code> which is the current
 %%       name of the column and the desired new name.</li>
@@ -84,25 +84,25 @@ start_link(Name, Args, Options) ->
 %% ----------------------------------------------------------------- %%
 
 -spec init(Args :: [option()]) ->
-    {ok, State :: aggr_var()}
+    {ok, State :: aggr_stddev()}
   | {error, {badarg, Term :: term()}}
 .
 
 %% @private
 %% @doc Initializes the aggregate node's internal state.
-init(Args) when is_list(Args) -> init(Args, #aggr_var{});
+init(Args) when is_list(Args) -> init(Args, #aggr_stddev{});
 
 init(_Args) -> {error, {badarg, not_options_list}}.
 
--spec delegate(Request :: atom(), State :: aggr_var()) ->
-    {ok, State :: aggr_var()}
+-spec delegate(Request :: atom(), State :: aggr_stddev()) ->
+    {ok, State :: aggr_stddev()}
 .
 
 %% @private
-%% @doc Passes the new variance down to its subscribers.
+%% @doc Passes the new standard deviation down to its subscribers.
 delegate(
     _Request = {tuple, Tuple}
-  , State = #aggr_var{
+  , State = #aggr_stddev{
         curr_count=CurrCount
       , curr_M2=CurrM2
       , curr_mean=CurrMean
@@ -111,7 +111,7 @@ delegate(
 ) ->
     {NewCount, NewM2, NewMean} =
         check_tuple(Tuple, Index, CurrCount, CurrM2, CurrMean)
-  , NewState = State#aggr_var{
+  , NewState = State#aggr_stddev{
         curr_count = NewCount
       , curr_M2 = NewM2
       , curr_mean = NewMean
@@ -121,7 +121,7 @@ delegate(
 
 delegate(
     _Request = {tuples, Tuples}
-  , State = #aggr_var{
+  , State = #aggr_stddev{
         curr_count=CurrCount
       , curr_M2=CurrM2
       , curr_mean=CurrMean
@@ -135,7 +135,7 @@ delegate(
       , {CurrCount, CurrM2, CurrMean}
       , Tuples
     )
-  , NewState = State#aggr_var{
+  , NewState = State#aggr_stddev{
         curr_count = NewCount
       , curr_M2 = NewM2
       , curr_mean = NewMean
@@ -145,8 +145,8 @@ delegate(
 
 %% @doc Receives the new set of valid indexes and sets it as part
 %%      of the state.
-delegate(_Request = {index, Index}, State = #aggr_var{}) ->
-    NewState = State#aggr_var{index=Index}
+delegate(_Request = {index, Index}, State = #aggr_stddev{}) ->
+    NewState = State#aggr_stddev{index=Index}
   , {ok, NewState}
 ;
 
@@ -160,10 +160,10 @@ delegate(_Request, State) ->
 
 -spec delegate(
     Request :: atom()
-  , State :: aggr_var()
+  , State :: aggr_stddev()
   , Extras :: list()
 ) ->
-    {ok, NewState :: aggr_var()}
+    {ok, NewState :: aggr_stddev()}
 .
 
 delegate(_Request, State, _Extras) ->
@@ -174,17 +174,17 @@ delegate(_Request, State, _Extras) ->
 
 -spec compute_schema(
     InputSchemas :: [ydb_plan_node:ydb_schema()]
-  , State :: aggr_var()
+  , State :: aggr_stddev()
 ) ->
     {ok, OutputSchema :: ydb_plan_node:ydb_schema()}
   | {error, {badarg, InputSchemas :: [ydb_plan_node:ydb_schema()]}}
 .
 
-%% @doc Returns the output schema of the var aggregate based upon the
-%%      supplied input schemas. Expects a single schema.
-compute_schema([Schema], #aggr_var{column=Column}) ->
+%% @doc Returns the output schema of the stddev aggregate based upon
+%%      the supplied input schemas. Expects a single schema.
+compute_schema([Schema], #aggr_stddev{column=Column}) ->
     {Index, NewSchema} =
-        ydb_aggr_utils:compute_new_schema(Schema, Column, "VAR")
+        ydb_aggr_utils:compute_new_schema(Schema, Column, "STDDEV")
     % Inform self of index to check for.
   , ydb_plan_node:relegate(
         erlang:self()
@@ -193,7 +193,7 @@ compute_schema([Schema], #aggr_var{column=Column}) ->
   , {ok, NewSchema}
 ;   
 
-compute_schema(Schemas, #aggr_var{}) ->
+compute_schema(Schemas, #aggr_stddev{}) ->
     {error, {badarg, Schemas}}
 .
 
@@ -201,23 +201,23 @@ compute_schema(Schemas, #aggr_var{}) ->
 %%%  private functions                                              %%%
 %%% =============================================================== %%%
 
--spec init([option()], State :: aggr_var()) ->
-    {ok, State :: aggr_var()}
+-spec init([option()], State :: aggr_stddev()) ->
+    {ok, State :: aggr_stddev()}
   | {error, {badarg, Term :: term()}}
 .
 
 %% @private
 %% @doc Parses initializing arguments to set up the internal state of
-%%      the variance aggregate node.
-init([], State = #aggr_var{}) ->
+%%      the standard deviation aggregate node.
+init([], State = #aggr_stddev{}) ->
     {ok, State}
 ;
 
-init([{column, Column} | Args], State = #aggr_var{}) ->
-    init(Args, State#aggr_var{column=Column})
+init([{column, Column} | Args], State = #aggr_stddev{}) ->
+    init(Args, State#aggr_stddev{column=Column})
 ;
 
-init([Term | _Args], #aggr_var{}) ->
+init([Term | _Args], #aggr_stddev{}) ->
     {error, {badarg, Term}}
 .
 
@@ -232,9 +232,9 @@ init([Term | _Args], #aggr_var{}) ->
 ) -> {NewCount :: integer(), NewM2 :: float(), NewMean :: float()}.
 
 %% @private
-%% @doc Selects only the necessary column required for finding the var
-%%      and computes the new variance. Updates the current count, M2,
-%%      and mean.
+%% @doc Selects only the necessary column required for finding the 
+%%      stddev and computes the new standard deviation. Updates the 
+%%      current count, M2, and mean.
 check_tuple(
     Tuple=#ydb_tuple{data=Data}
   , Index
@@ -243,8 +243,9 @@ check_tuple(
   , CurrMean
 ) ->
     RelevantData = element(Index, Data)
-  , {NewM2, NewMean, Var} = get_var(CurrCount, CurrM2, CurrMean, RelevantData)
-  , NewTuple = Tuple#ydb_tuple{data=list_to_tuple([Var])}
+  , {NewM2, NewMean, StdDev} =
+        get_stddev(CurrCount, CurrM2, CurrMean, RelevantData)
+  , NewTuple = Tuple#ydb_tuple{data=list_to_tuple([StdDev])}
   , ydb_plan_node:notify(
         erlang:self()
       , {tuple, NewTuple}
@@ -252,25 +253,25 @@ check_tuple(
   , {CurrCount + 1, NewM2, NewMean}
 .    
 
--spec get_var(
+-spec get_stddev(
     CurrCount :: integer()
   , CurrM2 :: float()
   , CurrMean :: float()
   , NewNum :: number())
--> {NewM2 :: float(), NewMean :: float(), Var :: float()}.
+-> {NewM2 :: float(), NewMean :: float(), StdDev :: float()}.
     
-%% @doc Gets the variance.
-get_var(0, _CurrM2, _CurrMean, NewNum) ->
+%% @doc Gets the standard deviation.
+get_stddev(0, _CurrM2, _CurrMean, NewNum) ->
    {0, NewNum, 0}
 ;
 
-get_var(CurrCount, CurrM2, CurrMean, NewNum) ->
+get_stddev(CurrCount, CurrM2, CurrMean, NewNum) ->
     NewCount = CurrCount + 1
   , Delta = NewNum - CurrMean
   , NewMean = CurrMean + Delta / NewCount
   , NewM2 = CurrM2 + Delta * (NewNum - NewMean)
   , Var = NewM2 / CurrCount
-  , {NewM2, NewMean, Var}
+  , {NewM2, NewMean, math:sqrt(Var)}
 .
 
 %%% =============================================================== %%%
@@ -280,12 +281,12 @@ get_var(CurrCount, CurrM2, CurrMean, NewNum) ->
 -ifdef(TEST).
 init_test() ->
     ?assertMatch(
-        {ok, #aggr_var{column=[first]}}
-      , init([], #aggr_var{column=[first]})
+        {ok, #aggr_stddev{column=[first]}}
+      , init([], #aggr_stddev{column=[first]})
     )
   , ?assertMatch(
         {error, {badarg, bad}}
-      , init([bad], #aggr_var{})
+      , init([bad], #aggr_stddev{})
     )
 .
 -endif.
