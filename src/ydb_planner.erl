10e256e58b92da64296db7ebe28345397e0e4d6b
Max Hirschhorn <maxh@caltech.edu>
2013-05-04 21:22:22 -0700

    Added support for average in planner.
    Left this aggregate operation out from last commit by accident.

--- src/ydb_planner.erl
+++ src/ydb_planner.erl
@@ -209,6 +209,66 @@ make(
 ;
 
 make(
+    {Type = avg, Column, Prev}
+  , History
+  , Result
+) when
+    is_tuple(Prev)
+  , is_list(Result)
+  ->
+    case make(Prev, History, Result) of
+        {ok, {PrevId, NewHistory, NewResult}} ->
+            CurrId = get_id(Type, NewHistory)
+          , Listen = prepare_listen(PrevId)
+
+          , ChildSpec = prepare_child_spec(CurrId, {
+                ydb_avg, start_link, [
+                    [{column, Column}]
+                  , [{listen, [Listen]}]
+                ]
+            })
+
+          , {ok, {
+                CurrId
+              , dict:update_counter(Type, 1, NewHistory)
+              , [ChildSpec|NewResult]}
+            }
+
+      ; {error, Reason} -> {error, Reason}
+    end
+;
+
+make(
+    {Type = avg_rel, Column, Prev}
+  , History
+  , Result
+) when
+    is_tuple(Prev)
+  , is_list(Result)
+  ->
+    case make(Prev, History, Result) of
+        {ok, {PrevId, NewHistory, NewResult}} ->
+            CurrId = get_id(Type, NewHistory)
+          , Listen = prepare_listen(PrevId)
+
+          , ChildSpec = prepare_child_spec(CurrId, {
+                ydb_avg_rel, start_link, [
+                    [{column, Column}]
+                  , [{listen, [Listen]}]
+                ]
+            })
+
+          , {ok, {
+                CurrId
+              , dict:update_counter(Type, 1, NewHistory)
+              , [ChildSpec|NewResult]}
+            }
+
+      ; {error, Reason} -> {error, Reason}
+    end
+;
+
+make(
     {Type = min, Column, Prev}
   , History
   , Result
