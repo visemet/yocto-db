6f7c6920a929bc4644b43965da1a2fa5f5269130
Max Hirschhorn <maxh@caltech.edu>
2013-05-04 18:20:51 -0700

    Began rewriting ydb_planner module.
    Inverted the nesting order of how queries are defined. Rather than
    going from the input stream to its listener and so on, the nested tuple
    is node in the plan that the one currently processed listens to.
    
    As written, this function make/1 is not tail-recursive, but could be
    done so using continuation-passing style.

--- src/ydb_planner.erl
+++ src/ydb_planner.erl
@@ -1,10 +1,21 @@
 -module(ydb_planner).
 
--export([make_nonjoin/1, make_no_branch/1]).
+-export([make/1, make_nonjoin/1, make_no_branch/1]).
 
 %%% =============================================================== %%%
 %%%  API                                                            %%%
 %%% =============================================================== %%%
+
+%-spec
+
+%% @doc TODO
+make(Query) when is_tuple(Query) ->
+    case make(Query, dict:new(), []) of
+        {ok, {_Id, _History, Result}} -> {ok, lists:reverse(Result)}
+
+      ; {error, Reason} -> {error, Reason}
+    end
+.
 
 %-spec
 
@@ -32,6 +43,85 @@ make_no_branch({input_stream, Query, Name})
 %%% =============================================================== %%%
 %%%  private functions                                              %%%
 %%% =============================================================== %%%
+
+%-spec
+
+%% @doc TODO
+make({input_stream, Name, Type}, History, Result) ->
+    {ok, {{Name, Type}, History, Result}}
+;
+
+make(
+    {Type = file_output, Filename, Prev}
+  , History
+  , Result
+) when
+    is_tuple(Prev)
+  , is_list(Result)
+  ->
+    case make(Prev, History, Result) of
+        {ok, {PrevId, NewHistory, NewResult}} ->
+            CurrId = get_id(Type, NewHistory)
+          , Listen = prepare_listen(PrevId)
+
+          , ChildSpec = prepare_child_spec(CurrId, {
+                ydb_file_output, start_link, [
+                    [{filename, Filename}]
+                  , [{listen, [Listen]}]
+                ]
+            })
+
+          , {ok, {
+                CurrId
+              , dict:update_counter(Type, 1, NewHistory)
+              , [ChildSpec|NewResult]}
+            }
+
+      ; {error, Reason} -> {error, Reason}
+    end
+;
+
+make(Query, _History, _Result) ->
+    {error, {badarg, Query}}
+.
+
+%% ----------------------------------------------------------------- %%
+
+%-spec
+
+%% @doc TODO
+get_id(Type, History) ->
+    case dict:find(Type, History) of
+        {ok, Value} -> {Type, Value}
+
+      ; error -> {Type, 0}
+    end
+.
+
+%-spec
+
+%% @doc TODO
+prepare_listen(PlanId) ->
+    case PlanId of
+        % Listens to an input stream
+        {InputStream, BranchType}
+          when
+            is_atom(InputStream)
+          , is_atom(BranchType)
+          ->
+            {ydb_sup_utils:get_branch_pid(InputStream), BranchType}
+
+        % Listens to another node in the query
+      ; {PlanType, PlanValue}
+          when
+            is_atom(PlanType)
+          , is_integer(PlanValue)
+          ->
+            ydb_sup_utils:pid_fun(PlanId)
+    end
+.
+
+%% ----------------------------------------------------------------- %%
 
 %-spec
 
