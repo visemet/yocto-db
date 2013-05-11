68e92afff8739341ab2e69dcead28bb29484924b
ksuraesh <ksuraesh@caltech.edu>
2013-05-11 03:39:20 -0700

    Added more funs for aggregates. - Added avg, var, std_dev.

--- src/ydb_aggr_funs.erl
+++ src/ydb_aggr_funs.erl
@@ -1,12 +1,15 @@
-%% @author Angela Gong <anjoola@anjoola.com>
+%% @author Angela Gong, Kalpana Suraesh
+%%         <anjoola@anjoola.com, ksuraesh@caltech.edu>
 
 %% @doc This module contains functions for input into the aggregate
 %%      node.
 -module(ydb_aggr_funs).
 
 -export([identity/1]).
--export([count_single/2, count_all/1, sum_single/2, sum_all/1]).
+-export([count_single/2, count_all/1, sum_single/2, sum_all/1,
+         avg_single/2, avg_all/1]).
 -export([min_single/2, min_all/1, max_single/2, max_all/1]).
+-export([var_single/2, var_all/1, stddev_single/2, stddev_all/1]).
 
 %% @headerfile "ydb_plan_node.hrl"
 -include("ydb_plan_node.hrl").
@@ -62,10 +65,23 @@ sum_all(List) ->
 
 -spec avg_single(List :: [term()], Previous :: [integer()]) -> term().
 
-%% @doc Computes the average over a single diff.
+%% @doc Computes the average over a single diff. This is the
+%%      PartialFun.
 avg_single(List, []) ->
-    lists:sum(List) / lists:length(List)
+    {lists:length(List), lists:sum(List)}
+;
+avg_single(List, [{PrevCount, PrevSum}]) ->
+    {lists:length(List) + PrevCount, lists:sum(List) + PrevSum}
 .
+
+-spec avg_all(List :: [term()]) -> term().
+
+%% @doc Computes the avg over all the diffs. This is the AggrFun.
+avg_all(List) ->
+    {Count, Sum} = lists:last(List) % get the NewPartial
+  , Sum / Count % compute the average
+.
+
 
 %% ----------------------------------------------------------------- %%
 
@@ -109,6 +125,60 @@ max_all(List) ->
 
 %% ----------------------------------------------------------------- %%
 
+-spec var_single(List :: [term()], Previous :: [integer()]) -> term().
+
+%% @doc Computes the variance over a single diff. This is the
+%%      PartialFun.
+var_single(List, []) ->
+    {
+        lists:length(List)
+      , lists:sum(List)
+      , lists:sum(lists:map(fun(X) -> X*X end, List))
+    }
+;
+var_single(List, [{PrevCount, PrevSum, PrevSumSq}]) ->
+    {
+        lists:length(List) + PrevCount
+      , lists:sum(List) + PrevSum
+      , lists:sum(lists:map(fun(X) -> X*X end, List)) + PrevSumSq
+    }
+.
+
+-spec var_all(List :: [term()]) -> term().
+
+%% @doc Computes the avg over all the diffs. This is the AggrFun.
+var_all(List) ->
+    {Count, Sum, SumSq} = lists:last(List) % get the NewPartial
+  , (Count * SumSq - Sum * Sum)/(Count * Count) % compute var
+.
+
 %% ----------------------------------------------------------------- %%
+
+-spec stddev_single(List :: [term()], Previous :: [integer()]) -> term().
+
+%% @doc Computes the stddev over a single diff. This is the
+%%      PartialFun.
+stddev_single(List, []) ->
+    {
+        lists:length(List)
+      , lists:sum(List)
+      , lists:sum(lists:map(fun(X) -> X*X end, List))
+    }
+;
+stddev_single(List, [{PrevCount, PrevSum, PrevSumSq}]) ->
+    {
+        lists:length(List) + PrevCount
+      , lists:sum(List) + PrevSum
+      , lists:sum(lists:map(fun(X) -> X*X end, List)) + PrevSumSq
+    }
+.
+
+-spec stddev_all(List :: [term()]) -> term().
+
+%% @doc Computes the stddev over all the diffs. This is the AggrFun.
+stddev_all(List) ->
+    {Count, Sum, SumSq} = lists:last(List)
+  , math:sqrt((Count * SumSq - Sum * Sum)/(Count * Count))
+.
 
 %% ----------------------------------------------------------------- %%
